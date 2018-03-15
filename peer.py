913905189d2576525f5c6a47ca1cc798ef052454
FairLight8 <marino@gamesfera.com>
2018-03-15 17:32:03 +0100

    Version funcionando sin pulir

--- peer.py
+++ peer.py
@@ -3,53 +3,100 @@ import socket
 import pickle
 import sys
 
-comandos_server = ["NEW"]
 
 puertoserver=input("Introduzca el puerto del servidor:")
 
 ipserver=input("Introduzca la direccion IP del servidor:")
 
+
+
 tupla_server = ipserver,int(puertoserver)
 
-socket_envio = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-listener_chat = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+socket_server = socket.socket() #ES TCP
+listener_chat = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) #ES UDP
 
 listener_chat.bind(("localhost",0))
 
-socket_envio.sendto((str)(listener_chat.getsockname()[1]).encode(),tupla_server)
+socket_server.connect(tupla_server)
+socket_server.sendall(("CONEX "+str(listener_chat.getsockname()[1])).encode())
 
-listapickle,tupla_server=socket_envio.recvfrom(1024)
 
-lista_peers=pickle.loads(listapickle)
+cadena,tupla_server=listener_chat.recvfrom(32)
+
+lista_peers=[]
+
+if cadena.decode() == "NAME" and tupla_server[0] == socket_server.getpeername()[0]:
+    listener_chat.sendto(input("Introduzca nombre: ").encode(),tupla_server)
+    listapickle,tupla_server=listener_chat.recvfrom(1024)
+    lista_peers=pickle.loads(listapickle)
+    socket_server.close()
+
+else:
+    print("No nos ha respondido el server")
+    sys.exit(-1)
+
+print(lista_peers)
+
 
 set_sd = [sys.stdin,listener_chat]
+try:
+    while True:
+        listos=select.select(set_sd,[],[])
+        for i in listos[0]:
+            if i is sys.stdin:     #es nuestro teclado
+                cadena=input()
+                for peer in lista_peers:
+                    if peer[1] != listener_chat.getsockname():
+                        listener_chat.sendto(cadena.encode(), peer[1])
 
-while True:
-    listos=select.select(set_sd,[],[])
-    for i in listos[0]:
-        if i is sys.stdin:     #es nuestro teclado
-            cadena=input()
-            for j in lista_peers:
-                listener_chat.sendto(cadena.encode(),j)
+            if i is listener_chat:                                   #es un mensaje entrante
+                recib,dir_peer=listener_chat.recvfrom(1024)
+                print("DIr peer ahora es")
+                print(dir_peer)
+                flag_server = True
+                for peer in lista_peers:
+                    if dir_peer == peer[1]:  # hemos encontrado el peer que nos lo ha mandado
+                        print("\t\t\t" + peer[0] + " : " + recib.decode())
+                        flag_server=False
+                        break
+                if flag_server == True:
+                    print("Hemos recibido una lista del server")
+                    lista_peers_temp = pickle.loads(recib)
+                    if len(lista_peers_temp) > len(lista_peers):
+                        print(lista_peers_temp[-1][0] + " se ha unido a la sala")
+                    else:
+                        flag=True
+                        for k in range(len(lista_peers_temp)):
+                            print(lista_peers_temp[k])
+                            print(lista_peers[k])
+                            if lista_peers_temp[k] != lista_peers[k]: #Si encontramso una diferencia recorriendo es que se ha ido alguien
+                                print(lista_peers[k][0] + " ha abandonado la sala")
+                                break
+                                flag=False
+                        if flag:
+                            print(lista_peers[-1][0] + " ha abandonado la sala")
+                    lista_peers=lista_peers_temp
 
-        if i is listener_chat:                                   #es un mensaje entrante
-            cadena,tuplapeer=listener_chat.recvfrom(1024)
-            cadena=cadena.decode()
-            if tuplapeer not in lista_peers:    #mensaje entrante de alguien no en el server
-                if tuplapeer == tupla_server:   #es información del propio server
-                    if cadena in comandos_server:    #es un mensaje del server
-                        new,tuplapeer=listener_chat.recvfrom(1024)
-                        lista_peers.append(pickle.loads(new))
-                    else:                               #hemos recibido algo que no esperabamos
-                        print("Error en recepcion del server")
-                else:                           #es informacion de alguien que ni es el server ni un peer registrado
-                    print("Hemos recibido una cadena de alguien no registrado")
-                    print(lista_peers)
-                    print(tuplapeer)
-                    
-            else:
-                print("He recibido esta cadena")                               #mensaje registrado
-                print(cadena)
+except KeyboardInterrupt:
+
+    tupla_server = ipserver, int(puertoserver)
+
+    socket_server = socket.socket()  # ES TCP
+    socket_server.connect(tupla_server)
+    socket_server.send(("LEAVE " + str(listener_chat.getsockname()[1])).encode())
+    print("Hasta luego :)")
+    sys.exit(0)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- server.py
+++ server.py
@@ -2,32 +2,59 @@ import socket
 import select
 import pickle
 
-puertoserver=input("Introduzca el puerto:")     
+def actualizarlistaalospeers(lista):
+    for peer in lista_peers:
+        s_descriptor.sendto(pickle.dumps(lista_peers), peer[1])
 
-listener = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-listener.bind(("localhost",int(puertoserver)))
+if __name__ == "__main__":
 
-listener.listen(5)
+    puertoserver=input("Introduzca el puerto:")
 
-lista_peers=[]
-lista_peers_proceso=[]      #cada peer es una tupla direccion,nombre, de los cuales la primera, la direccion tambien es uan tupla
-lista_sockets=[listener]
+    listener = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-while True:
-    listos=select.select(lista_sockets,[],[])
-    for s_descriptor in listos:
-        if s_descriptor == listener:    #es una nueva conexion al socket
-            nuevo_socket,nueva_dir = listener.accept()
-            cadena = nuevo_socket.recv(32).decode()
-            if cadena[:5] == "CONEX":   #alguien quiere entrar a la sala
-                lista_sockets.append(socket.socket(socket.AF_INET,socket.SOCK_DGRAM))
-            elif cadena[:5] == "LEAVE": #alguien quiere salir de la sala
-            
-            else:                           #no reconocemos el mensaje
-            
-        else:                           #es socket abierto de alguien que esta estableciendo conexion
+    listener.bind(("localhost",int(puertoserver)))
 
+    listener.listen(5)
+
+    lista_peers=[]          #cada peer es una tupla de nombre y direccion, la cual tambien es una tupla
+    lista_sockets=[listener]
+
+    while True:
+        listos=select.select(lista_sockets,[],[])
+        for s_descriptor in listos[0]:
+            print("Entrado en el for")
+            print(s_descriptor)
+            print(listener)
+            if s_descriptor == listener:    #es una nueva conexion al socket
+                print("Es el listener")
+                nuevo_socket,nueva_dir = listener.accept()
+                cadena = nuevo_socket.recv(32).decode()
+                print("Ha recibido algo"+cadena)
+                if cadena[:5] == "CONEX":   #alguien quiere entrar a la sala
+                    print("Ha entrado en el conex")
+                    nuevo_socket_udp=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+                    nuevo_socket_udp.sendto("NAME".encode(),(nueva_dir[0],int(cadena[6:])))
+                    print("He mandado la cadena a ")
+                    print((nueva_dir[0],int(cadena[6:])))
+                    lista_sockets.append(nuevo_socket_udp)
+                elif cadena[:5] == "LEAVE": #alguien quiere salir de la sala
+                    print("Ha entrado en el leave")
+                    puerto_desconexion=int(cadena[6:])
+                    for peer in lista_peers:
+                        if peer[1][1] == puerto_desconexion:
+                            lista_peers.remove(peer)
+                            for peer in lista_peers:
+                                socket.socket(socket.AF_INET,socket.SOCK_DGRAM).sendto(pickle.dumps(lista_peers), peer[1])  #creo un socket udp solo para mandarlo
+
+                else:                           #no reconocemos el mensaje
+                    print("QUEEEESESTOLOCO")
+            else:                           #es socket abierto UDP de alguien que esta estableciendo conexion
+                print("Ha entrado en el else")
+                recibo_udp=s_descriptor.recvfrom(32)
+                lista_peers.append((recibo_udp[0].decode(),recibo_udp[1]))  #añadimos a la lista le nuevo peer
+                for peer in lista_peers:
+                    s_descriptor.sendto(pickle.dumps(lista_peers),peer[1])
 
 
 
